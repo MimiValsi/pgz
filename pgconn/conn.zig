89e85a0d8b354d45f524272900303a4e14538b10
Miguel Da Silva Ferreira <miguel@dasilvaf.net>
2025-10-15 09:35:27 +0200

    test some connection typs

--- pgconn/conn.zig
+++ pgconn/conn.zig
@@ -1,29 +1,198 @@
 const std = @import("std");
 const printd = std.debug.print;
 const cfg = @import("./config.zig");
+const dft = @import("defaults.zig");
 const mem = std.mem;
 const expect = std.testing.expect;
 
 const Pgconn = struct {
-    conn: std.net.Stream,
+    stream: *std.net.Stream,
+
+    config: *cfg.Config,
 };
 
-test "database url tokenizing" {
-    const dataURL = "postgres://user:pass@host:port/database";
+// NOTE: At the moment, simple path check if it's unix socket or not.
+// url: postgres://user:pass@localhost:port/database
+// 1st tokenize url then check type of connection on host
+// port will be appended on socket path or IP port
+pub fn connect(url: []const u8, alloc: std.mem.Allocator) !*Pgconn {
+    const config = try cfg.Config.createConfigAlloc(url, alloc);
 
-    var f = mem.tokenizeAny(u8, dataURL, ":/@");
-
-    while (f.next()) |val| {
-        try expect(@TypeOf(val) == []const u8);
-    }
-
-    f.reset();
-    try expect(mem.eql(u8, f.next().?, "postgres"));
-    try expect(mem.eql(u8, f.next().?, "user"));
-    try expect(mem.eql(u8, f.next().?, "pass"));
-    try expect(mem.eql(u8, f.next().?, "host"));
-    try expect(mem.eql(u8, f.next().?, "port"));
-    try expect(mem.eql(u8, f.next().?, "database"));
-    try expect(f.next() == null);
+    // return &.{ .stream = , .config = config };
 }
 
+// fn setStream(url: []const u8)
+
+test "config" {
+    const url = "postgres://user:pass@localhost:port/database";
+    var buf: [1024]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buf);
+    const alloc = fba.allocator();
+    connect(url, alloc);
+}
+
+// startup message doesn't have initial message-type byte.
+// buf: |____|____|__...__|
+//      0    4    8       N
+//      ^    ^    ^
+//      |    |    \ Message...
+//      |    |
+//      |    \ Next 4 bytes, contains the protocol version (196608)
+//      |
+//      \ First 4 bytes, contains the length of the message plus it self.
+//          total length: message length + 4 first bytes + 4 bytes from protocol?
+
+// test "connection" {
+//     const path = "/run/postgresql/.s.PGSQL.5432"; // It works!
+//     // const path = "127.0.0.1"; // It works!
+//     // const path = "::1"; // This works!
+
+//     const pgconn = try connect(path, 5432);
+//     defer pgconn.stream.close();
+
+//     var buf: [128]u8 = undefined;
+//     var startUpMessage = std.ArrayList(u8).initBuffer(&buf);
+
+//     const user = "user\x00postgres\x00\x00";
+//     const db = "database\x00pgzdb\x00\x00";
+
+//     const data_len = @as(i32, @intCast(user.len)); // <- Get data length.
+
+//     var length_bytes: [4]u8 = undefined;
+//     std.mem.writeInt(i32, &length_bytes, data_len + 8, .big);
+//     startUpMessage.appendSliceAssumeCapacity(&length_bytes);
+
+//     var proto_version: [4]u8 = undefined;
+//     std.mem.writeInt(i32, &proto_version, 196608, .big);
+//     startUpMessage.appendSliceAssumeCapacity(&proto_version);
+
+//     startUpMessage.appendSliceAssumeCapacity(user); // <- append user
+//     startUpMessage.appendSliceAssumeCapacity(db);   // <- append database
+
+//     var writer = pgconn.stream.writer(&.{});
+//     var writer_ptr = &writer.interface;
+
+//     try writer_ptr.writeAll(startUpMessage.items[0..4]); // <- 1st sent: message length
+//     try writer_ptr.writeAll(startUpMessage.items[4..8]); // <- 2nd sent: proto version
+//     try writer_ptr.writeAll(startUpMessage.items[8..]); // <- 3rd sent: the message
+
+//     printArrayListItems(startUpMessage);
+
+//     try writer_ptr.flush();
+
+//     var r_buf: [10000]u8 = undefined;
+//     var reader: std.net.Stream.Reader = pgconn.stream.reader(&r_buf);
+//     const r_in: *std.Io.Reader = reader.interface(); // This is meant to return *Io.Reader
+
+//     var len: u32 = 1;
+//     while (true) {
+//         const byte = try r_in.take(len);
+//         if (len == 1) {
+//             switch (byte[0]) {
+//                 'S' => {
+//                     parameters();
+//                     len = 4;
+//                 },
+//                 'R' => {
+//                     authentication();
+//                     len = 4;
+//                 },
+//                 // 'K' => {
+//                 //     backendKeyData();
+//                 //     len = 4;
+//                 // },
+//                 // 'C' => {
+//                 //     commandComplete();
+//                 //     len = 4;
+//                 // },
+//                 // 'G' => {
+//                 //     copyInResponse();
+//                 // },
+//                 // 'H' => {
+//                 //     copyOutResponse();
+//                 // },
+//                 // 'W' => {
+//                 //     copyBothResponse();
+//                 // },
+//                 // 'D' => {
+//                 //     dataRow();
+//                 // },
+//                 // 'I' => {
+//                 //     emptyQueryResponse();
+//                 // },
+//                 // 'E' => {
+//                 //     errorMsg();
+//                 //     len = 4;
+//                 // },
+//                 // 'V' => {
+//                 //     functionCallResponse();
+//                 // },
+//                 // 'v' => {
+//                 //     negociateProtoVersion();
+//                 // },
+//                 // 's' => {
+//                 //     portalSuspended();
+//                 // },
+//                 // 'n' => {
+//                 //     noData();
+//                 // },
+//                 // 'Z' => {
+//                 //     readyForQuery();
+//                 // },
+//                 // 'T' => {
+//                 //     rowDescription();
+//                 // },
+//                 // 'A' => {
+//                 //     notifResponse();
+//                 // },
+//                 // 't' => {
+//                 //     paramDescription();
+//                 // },
+//                 // 'S' => {
+//                 //     paramStatus();
+//                 // },
+//                 // '1' => {
+//                 //     parseComplete();
+//                 // },
+//                 // '2' => {
+//                 //     bindComplete();
+//                 //     len = 4;
+//                 // },
+//                 // '3' => {
+//                 //     closeComplete();
+//                 //     len = 4;
+//                 // },
+//                 else => printd("Unknown identifier!\n", .{}),
+//             }
+//         }
+
+//         // if (len == 4) {
+//         //     std.mem.writeInt(i32, byte, value, .big);
+//         // }
+//         printd("{s} ", .{byte});
+//         break;
+//     }
+
+//     printd("\n", .{});
+// }
+
+fn errorMsg() void {
+    printd("error msg\n", .{});
+}
+fn authentication() void {
+    printd("authentication\n", .{});
+}
+
+fn parameters() void {
+    printd("parameters\n", .{});
+}
+fn backendKeyData() void {
+    printd("backend key data\n", .{});
+}
+
+fn printArrayListItems(items: std.ArrayList(u8)) void {
+    for (items.items) |item| {
+        printd("{x} ", .{item});
+    }
+    printd("\n\n", .{});
+}
