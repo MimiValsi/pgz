e3b7fd6db76734551f8d4835f2505725b896ee5a
Miguel Da Silva Ferreira <miguel@dasilvaf.net>
2025-10-15 09:36:18 +0200

    some defaults ideas

--- pgconn/defaults.zig
+++ pgconn/defaults.zig
@@ -1,11 +1,12 @@
 const std = @import("std");
 const printd = std.debug.print;
 const expect = std.testing.expect;
+const mem = std.mem;
 
 pub fn defaultSettings(alloc: std.mem.Allocator) !std.StringHashMap([]const u8) {
     var setting = std.StringHashMap([]const u8).init(alloc);
 
-    try setting.put("host", defaultHost());
+    try setting.put("host", defaultHost("host"));
     try setting.put("port", "5432");
 
     return setting;
@@ -18,11 +19,14 @@ test "default settings" {
     var ds = try defaultSettings(page);
     defer ds.deinit();
 
-    try expect(std.mem.eql(u8, ds.get("host").?, "localhost"));
+    try expect(std.mem.eql(u8, ds.get("host").?, "host"));
     try expect(std.mem.eql(u8, ds.get("port").?, "5432"));
 }
 
 // This may be populated with others options
-fn defaultHost() []const u8 {
-    return "localhost";
+pub fn defaultHost(path: []const u8) []const u8 {
+    if (mem.eql(u8, path, "/run/postgresql/.s.PGSQL.5432")) {
+        return "unix";
+    }
+    return path;
 }
